3222a1c2bcd13850feb1ab8c6bf94f321633f9d2
cybermaique <maiqued.18@gmail.com>
2022-06-03 21:55:00 -0300

    test update

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/atualizou', (request, response) => {
+    return response.json({message: 'Atualizou hehehe!'});
+})
+
 app.get('/', (request, response) => {
     return response.json({message: 'Server está rodando!!!'});
 })
